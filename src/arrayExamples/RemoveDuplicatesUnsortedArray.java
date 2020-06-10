b36b3cb1c29c3358a2f7fded69088ffc15970096
Manohara Chillara <chillara.rao@gmail.com>
2020-06-09 20:09:00 -0700

    Pushing Remove Duplicate unsorted array

---
+++ src/arrayExamples/RemoveDuplicatesUnsortedArray.java
@@ -0,0 +1,27 @@
+package arrayExamples;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesUnsortedArray {
+
+	public static void main(String[] args) {
+		int [] arr = {2,4,1,8,4,2,3,1,4,1};
+		int [] temp = new int [arr.length];
+		int j=0;
+		Arrays.sort(arr);
+		for(int i=0; i<arr.length-1;i++) {
+			if(arr[i] !=arr[i+1]) {
+				arr[j++] = arr[i];
+			  System.out.println("Value is : " +arr[j]);
+			}
+			
+		}
+		
+		arr[j++] = arr[arr.length-1];
+		for(int k=0; k<j; k++) {
+			System.out.println(arr[k] +"");
+		}
+
+	}
+
+}
